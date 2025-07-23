85fe84f57663d93cb088eaac35bac2295ffebff5
rosataker <pechin1219@gmail.com>
2025-07-23 11:38:14 +0800

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen bg-gradient-to-br from-pink-500 to-yellow-500 flex items-center justify-center text-white">
+    <h1 class="text-4xl font-bold">✅ Tailwind 啟用了！</h1>
+  </div>
+</template>
