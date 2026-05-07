f8e13bcb2f1e7e44692da4e9dfaf3e55b70a75aa
rosataker <pechin1219@gmail.com>
2026-05-07 22:29:39 +0800

    更新

--- app/components/character/CharacterBasic.vue
+++ app/components/character/CharacterBasic.vue
@@ -3,7 +3,7 @@
     <!-- 左欄 -->
     <div class="flex flex-col gap-2 flex-1">
       <div class="relative border-4 border-yellow-900 rounded-xl bg-white/80 shadow p-2 pt-5">
-        <div class="absolute -top-3 left-6 bg-white/80 px-2 text-yellow-900 font-bold tracking-widest text-x">姓名</div>
+        <div class="absolute -top-3 left-6 bg-white/80 px-2 text-yellow-900 font-bold tracking-widest text-x">角色姓名</div>
         <input v-model="form.name" class="w-full bg-transparent border-0 border-b-2 border-yellow-400 focus:outline-none text-yellow-900 text-base py-1" />
       </div>
       <div class="relative border-4 border-yellow-900 rounded-xl bg-white/80 shadow p-2 pt-5">
@@ -11,7 +11,7 @@
         <input v-model="form.house" class="w-full bg-transparent border-0 border-b-2 border-yellow-400 focus:outline-none text-yellow-900 text-base py-1" />
       </div>
       <div class="relative border-4 border-yellow-900 rounded-xl bg-white/80 shadow p-2 pt-5">
-        <div class="absolute -top-3 left-6 bg-white/80 px-2 text-yellow-900 font-bold tracking-widest text-x">角色</div>
+        <div class="absolute -top-3 left-6 bg-white/80 px-2 text-yellow-900 font-bold tracking-widest text-x">職責</div>
         <input v-model="form.role" class="w-full bg-transparent border-0 border-b-2 border-yellow-400 focus:outline-none text-yellow-900 text-base py-1" />
       </div>
       <div class="relative border-4 border-yellow-900 rounded-xl bg-white/80 shadow p-2 pt-5">
